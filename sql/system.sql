7d4cfb0a6e2c41ba83a5699e0f6f20c1fbb01dd4
Supermage9 <dbsrudgh123@naver.com>
2022-06-29 15:22:31 +0900

    0629

--- sql/system.sql
+++ sql/system.sql
@@ -1,7 +1,17 @@
 create user javauser identified by java;
 
-grant connect, resource to javauser;
+grant connect, RESOURCE to javauser;
 
 REVOKE connect, resource from javauser;
 
 grant create view TO SCOTT;
+
+
+
+create tablespace scott_data datafile 'c:\ora\scott_data.dbf' size 10m;
+
+create user backupuser IDENTIFIED by backup1234 default TABLESPACE scott_data;
+
+grant connect, resource to backupuser;
+
+commit;
---
+++ sql/사용자2.sql
@@ -0,0 +1,15 @@
+
+
+
+
+
+/*
+cmd - sqlplus - backupuser/backup1234 접속 - cd\ - cd ora - imp - backupuser/backup1234 접속 - 
+데이터만 임포트 yes - 임포트 파일에 c:\ora\scott_data.dmp 입력 - 
+
+cmd - cd\ - exp scott/tiger file=c:\ora\scott/tiger.dmp log=c:\ora\scott_data.log
+imp - scott/tiger
+
+
+
+*/
